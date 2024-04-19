e6396e0bcffc318b2b49cd31cb0878b82656baaf
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-19 10:36:34 +0500

    get user by username functionality added

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,10 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import UserHeader from "../components/UserHeader";
 import UserPost from "../components/UserPost";
+import useShowToast from "../hooks/useShowToast";
 
 const UserPage = () => {
+  const [user, setUser] = useState(null);
+  const { username } = useParams();
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setUser(data);
+      } catch (error) {
+        showToast("Error", error, "error");
+      }
+    };
+
+    getUser();
+  }, [username, showToast]);
+
+  if (!user) return null;
+
   return (
     <>
-      <UserHeader />
+      <UserHeader user={user} />
       <UserPost
         likes={1200}
         replies={481}
