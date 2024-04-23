7392fcdbdf76ca05430830084071ff79296e9e41
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-23 16:55:27 +0500

    show user posts and create user profile

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
-import { BsThreeDots } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Actions from "./Actions";
 import useShowToast from "../hooks/useShowToast";
+import { formatDistanceToNow } from "date-fns";
 
 const Post = ({ post, postedBy }) => {
   const [liked, setLiked] = useState(false);
+  const [user, setUser] = useState(null);
   const showToast = useShowToast();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getUser = async () => {
       try {
         const res = await fetch("/api/users/profile/" + postedBy);
         const data = await res.json();
-        console.log(data);
         if (data.error) {
           showToast("Error", data.error, "error");
           return;
         }
+        setUser(data);
       } catch (error) {
         showToast("Error", error.message, "error");
+        setUser(null);
       }
     };
 
@@ -28,66 +31,95 @@ const Post = ({ post, postedBy }) => {
   }, [postedBy, showToast]);
 
   return (
-    <Link to={"/markzuckerberg/post/1"}>
+    <Link to={`/${user?.username}/post/${post._id}`}>
       <Flex gap={3} mb={4} py={5}>
         <Flex flexDirection={"column"} alignItems={"center"}>
-          <Avatar size="md" name="Mark Zuckerberg" src="/zuck-avatar.png" />
+          <Avatar
+            size="md"
+            name={user?.name}
+            src={user?.profilePic}
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(`/${user?.username}`);
+            }}
+          />
           <Box w="1px" h={"full"} bg="gray.light" my={2}></Box>
           <Box position={"relative"} w={"full"}>
-            <Avatar
-              size="xs"
-              name="John Doe"
-              src="https://bit.ly/dan-abramov"
-              position={"absolute"}
-              top={"0px"}
-              left="15px"
-              padding={"2px"}
-            />
-            <Avatar
-              size="xs"
-              name="Segun Adebayo"
-              src="https://bit.ly/sage-adebayo"
-              position={"absolute"}
-              bottom={"0px"}
-              right="-5px"
-              padding={"2px"}
-            />
-            <Avatar
-              size="xs"
-              name="Prosper Otemuyiwa"
-              src="https://bit.ly/prosper-baba"
-              position={"absolute"}
-              bottom={"0px"}
-              left="4px"
-              padding={"2px"}
-            />
+            {post?.replies.length === 0 && <Text textAlign={"center"}>😟</Text>}
+
+            {post?.replies[0] && (
+              <Avatar
+                size="xs"
+                name="John Doe"
+                src={post?.replies[0].userProfilePic}
+                position={"absolute"}
+                top={"0px"}
+                left="15px"
+                padding={"2px"}
+              />
+            )}
+
+            {post?.replies[1] && (
+              <Avatar
+                size="xs"
+                name="Segun Adebayo"
+                src={post?.replies[1].userProfilePic}
+                position={"absolute"}
+                bottom={"0px"}
+                right="-5px"
+                padding={"2px"}
+              />
+            )}
+
+            {post?.replies[2] && (
+              <Avatar
+                size="xs"
+                name="Prosper Otemuyiwa"
+                src={post?.replies[2].userProfilePic}
+                position={"absolute"}
+                bottom={"0px"}
+                left="4px"
+                padding={"2px"}
+              />
+            )}
           </Box>
         </Flex>
         <Flex flex={1} flexDirection={"column"} gap={2}>
           <Flex justifyContent={"space-between"} w={"full"}>
             <Flex w={"full"} alignItems={"center"}>
-              <Text fontSize={"sm"} fontWeight={"bold"}>
-                markzuckerberg
+              <Text
+                fontSize={"sm"}
+                fontWeight={"bold"}
+                onClick={(e) => {
+                  e.preventDefault();
+                  navigate(`/${user?.username}`);
+                }}
+              >
+                {user?.username}
               </Text>
               <Image src="/verified.png" w={4} h={4} ml={1} />
             </Flex>
             <Flex gap={4} alignItems={"center"}>
-              <Text fontStyle={"sm"} color={"gray.light"}>
-                1d
+              <Text
+                fontSize={"xs"}
+                width={36}
+                textAlign={"right"}
+                color={"gray.light"}
+              >
+                {formatDistanceToNow(new Date(post.createdAt))} ago
               </Text>
-              <BsThreeDots />
             </Flex>
           </Flex>
 
-          <Text fontSize={"sm"}>{post.text}</Text>
-          {post.img && (
+          <Text fontSize={"sm"}>{post?.text}</Text>
+          {post?.img && (
             <Box
               borderRadius={6}
               overflow={"hidden"}
               border={"1px solid"}
               borderColor={"gray.light"}
             >
-              <Image src={post.img} w={"full"} />
+              <Image src={post?.img} w={"full"} />
             </Box>
           )}
 
@@ -106,7 +138,7 @@ const Post = ({ post, postedBy }) => {
               bg={"gray.light"}
             ></Box>
             <Text color={"gray.light"} fontSize={"sm"}>
-              {post.likes.length} likes
+              {post?.likes.length} likes
             </Text>
           </Flex>
         </Flex>
