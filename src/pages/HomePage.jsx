38e69f2764b3494498326f823bed59557c9f0cf7
sameerjohn1 <sameerprogrammer5@gmail.com>
2024-04-30 17:17:30 +0500

    initial changes occur

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,7 +24,6 @@ const HomePage = () => {
           showToast("Error", data.error, "error");
           return;
         }
-        console.log(data);
         setPosts(data);
       } catch (error) {
         showToast("Error", error.message, "error");
